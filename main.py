ea2fc3912b6713c4bde7c260a07853bc0ac515b9
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-10 12:35:54 +0330

    Create the First Row of the Calculator

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
-from curses import window
 import tkinter as tk
 
 window = tk.Tk()
 
+lbl_calc_result = tk.Label(
+    master=window,
+    text='0',
+    width=30,
+    height=3,
+)
+lbl_calc_result.grid(row=0, column=0, columnspan=4)
+
+calc_keys = [
+    {
+        'text' : '7',
+        'command' : lambda: print('7'),
+    },
+    {
+        'text' : '8',
+        'command': lambda: print('8'),
+    },
+    {
+        'text' : '9',
+        'command': lambda: print('9'),
+    },
+    {
+        'text' : '+',
+        'command': lambda: print('+'),
+    },
+]
+
+calc_keys_objs = []
 
 
-
+for calc_key_data in calc_keys:
+    btn = tk.Button(
+    master=window,
+    text=calc_key_data['text'],
+    command=calc_key_data['command'],
+    height=3
+    )
+    calc_keys_objs.append(btn) 
+    
+for i, calc_key_obj in enumerate(calc_keys_objs):
+    if i // 4 == 0:
+        calc_key_obj.grid(row= (i//4)+1, column= i%4, sticky='nsew')
+        
 
 window.mainloop()
